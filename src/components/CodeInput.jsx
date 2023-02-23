5afd2af77656172a18dfdeebe372a6c20f72996a
EmmGlezz <contact@emilianogonzalez.com>
2023-02-23 19:22:18 +1100

    Comments Cleanup

--- src/components/CodeInput.jsx
+++ src/components/CodeInput.jsx
@@ -2,83 +2,20 @@ import React, {useState, useEffect} from 'react'
 import './CodeInput.css'
 
 const CodeInput = () => {
-    // const [codeValue, setCodeValue] = useState("")
-    // const [inputHeight, setinputHeight] = useState("156px")
     const [width, setWidth] = useState(90)
-    // const [length, setLength] = useState(0)
     const [lastVal, setLastVal] = useState(0)
 
+    // Height autosizing 
     const inputHeight = (element, defaultHeight) => {
         if (element) {
             const target = element.target ? element.target : element;
             target.style.height = defaultHeight;
-            // console.log(target.scrollHeight)
             target.style.height = `${target.scrollHeight - 20}px`;
         }
     }
-    
-    // const inputWidth = (currentLength) => {
-        // if (width <= 680) {
-        //     console.log("Target Value", target.value.length)
-        //     if(target.value.length >= 6) {
-        //         target.style.textAlign = 'center'
-        //         setWidth(width + 10)
-        //         target.style.width = `${width}px`
-        //         // setLength(length + 1)
-        //     } else if (target.value.length > 0) {
-        //         target.style.textAlign = 'left'
-        //         setWidth(width - 10)
-        //         target.style.width = `${width}px`
-        //     }
-        // } 
-        
-        
-        
-    // }
-    
-    // useEffect(() => {
-    //     const increase = () => {
-    //         console.log('INCREASING')
-    //         // target.style.textAlign = 'center'
-    //         setWidth(width + 10)
-    //     }
-        
-    //     const decrease = () => {
-    //         console.log('DECREASING')
-    //         // target.style.textAlign = 'left'
-    //         setWidth(width - 10)
-    //     }
-        
-    //     setVal(length)
-    //     length < val ? decrease() : increase()
-    //     console.log("val", length)
-        
-    // }, [length])
-    
-    // useEffect(() => {
-    //     console.log("EFFECT", width)
-    //     length < val ? width - 10 : width + 10
-    // }, [width])
 
-    // const testF = (e) => {
-    //     console.log(e)
-    // }
-
-    // const inputWidth = (element, defaultWidth) => {
-    //     if (element) {
-    //         const target = element.target ? element.target : element;
-    //         console.log("Client Width", width)
-    //         // setWidth(width + 10)
-    //         // target.style.width = `${width}px`;
-            
-    //         console.log("Length", length)
-            
-                // target.style.width = defaultWidth;
-            
-            
-    //     }
-    // }
-
+    // Width autosizing
+    // Algorithm to be modified (Current has few bugs)
     const inputWidth = (e) => {
         
         console.log("Text value:", e.target.value)
@@ -91,9 +28,6 @@ const CodeInput = () => {
             e.target.style.width = `${width + ((charLength - lastVal)*10)}px`
             setWidth(width + ((charLength - lastVal)*10))
             console.log("Width Increased",  (width + ((charLength - lastVal)*10)))
-            // if(!()){
-                
-                // }
                 setLastVal(charLength)
             } else if (charLength < lastVal){
                 if(width > 100){
@@ -119,23 +53,6 @@ const CodeInput = () => {
             <textarea onChange={e => {
                 inputHeight(e, "64px")
                 inputWidth(e)
-                // testF(e)
-                
-                // console.log(length)
-                // console.log("E target Value", e.target.value.length)
-                
-                // if (e.target.value.length < length && e.target.value.length > 6) {
-                //     setLength(length - 1)
-                //     setWidth(width - 10)
-                    
-                //     e.target.style.width = `${width}px`
-                    
-                //     if (e.target.value.length < 6) {
-                //         e.target.style.textAlign = 'left'
-                //     }
-                // }
-                    // setLength(e.target.value.length)
-                // inputWidth(e, "90px")
                 }} />
         </div>
     </div>
